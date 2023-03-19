648d8dcf5623a1798bd734b529712ff5662e734a
juyingnan <bunny_gg@hotmail.com>
2023-03-18 22:47:44 -0700

    added some functions

--- test3.py
+++ test3.py
@@ -1,7 +1,7 @@
 import sys
 import json
 from PyQt5.QtWidgets import QWidget, QApplication, QMainWindow, QPushButton, QVBoxLayout, QHBoxLayout, QTextEdit, \
-    QSplitter
+    QSplitter, QComboBox, QLabel
 from PyQt5.QtGui import QColor, QFont
 
 # Note: The openai-python library support for Azure OpenAI is in preview.
@@ -11,8 +11,7 @@ import openai
 
 def read_config_file(config_file_path):
     with open(config_file_path, 'r') as f:
-        config_data = json.load(f)
-    return config_data
+        return json.load(f)
 
 
 config_data = read_config_file('config.json')
@@ -23,9 +22,11 @@ openai.api_key = config_data['api_key']
 openai_key = config_data['openai_key']
 default_model = config_data['default_model']
 default_callback_num = config_data['default_callback_num']
+model_names = config_data['model_names']
 
 user_input_color = QColor(0, 128, 0)  # green
 ai_response_color = QColor(0, 0, 255)  # blue
+system_message_color = QColor(0, 0, 0)  # black
 
 
 # defining a function to create the prompt from the system message and the messages
@@ -47,6 +48,10 @@ system_message_instance = system_message_template.format("You are an AI assistan
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
+        # initialize the parameters
+        self.model = default_model
+        self.callback_num = default_callback_num
+
         self.setWindowTitle("GPT Chat")
         self.resize(1000, 1000)
 
@@ -76,6 +81,32 @@ class MainWindow(QMainWindow):
         self.submit_button = QPushButton("Send")
         self.submit_button.clicked.connect(self.on_user_input)
 
+        # create the clear button
+        self.clear_button = QPushButton('Clear history')
+        self.clear_button.clicked.connect(self.clear_history)
+
+        # create a dropdown menu for model
+        self.comboBox_model = QComboBox(self)
+        for model_name in model_names:
+            self.comboBox_model.addItem(model_name)
+        self.comboBox_model.activated[str].connect(self.onModelSelectionActivated)
+
+        # create a label to display the selected model
+        self.label_model = QLabel(self)
+        self.label_model.setText(f"Selected model:")
+
+        # create a dropdown menu for history length
+        self.comboBox_history = QComboBox(self)
+        for i in range(1, 11):
+            self.comboBox_history.addItem(str(i))
+        self.comboBox_history.addItem(str(999))
+        self.comboBox_history.setCurrentIndex(self.callback_num - 1)
+        self.comboBox_history.activated[str].connect(self.onHistoryLengthSelectionActivated)
+
+        # create a label to display the selected model
+        self.label_history = QLabel(self)
+        self.label_history.setText(f"History length:")
+
         # create the layout
         layout = QVBoxLayout()
         layout.addWidget(splitter)
@@ -83,6 +114,11 @@ class MainWindow(QMainWindow):
         input_layout = QHBoxLayout()
         # input_layout.addWidget(self.user_input)
         input_layout.addWidget(self.submit_button)
+        input_layout.addWidget(self.label_model)
+        input_layout.addWidget(self.comboBox_model)
+        input_layout.addWidget(self.label_history)
+        input_layout.addWidget(self.comboBox_history)
+        input_layout.addWidget(self.clear_button)
         layout.addLayout(input_layout)
 
         # create the widget to hold the layout
@@ -95,9 +131,24 @@ class MainWindow(QMainWindow):
         # initialize the messages list
         self.messages = []
 
-        # initialize the parameters
-        self.model = default_model
-        self.callback_num = default_callback_num
+    # dropdown menu activated function - model selection
+    def onModelSelectionActivated(self, text):
+        # call the selected GPT model based on the user's selection
+        self.model = text
+        # self.label_model.setText(f"Selected model: {text}")
+        self.chat_display.append(f"Current model changed to: {text}")
+
+    # dropdown menu activated function - model selection
+    def onHistoryLengthSelectionActivated(self, text):
+        # call the selected GPT model based on the user's selection
+        self.callback_num = int(text)
+        # self.label_model.setText(f"Selected model: {text}")
+        self.chat_display.append(f"Current history changed to: {text}")
+
+    def clear_history(self):
+        self.messages = []
+        self.chat_display.clear()
+        self.chat_display.append(f"Chat history cleared. You can begin a new chat session.")
 
     # function to handle user input
     def on_user_input(self):
@@ -109,6 +160,7 @@ class MainWindow(QMainWindow):
         # update the chat display widget
         self.chat_display.setTextColor(user_input_color)
         self.chat_display.append("You: " + user_input)
+        self.chat_display.setTextColor(system_message_color)
 
         # add the user message to the messages list
         self.messages.append({"sender": "user", "text": user_input})
@@ -137,7 +189,8 @@ class MainWindow(QMainWindow):
         # update the chat display widget
         self.chat_display.setTextColor(ai_response_color)
         self.chat_display.append("AI: " + ai_response)
-        self.chat_display.append("----\n")
+        self.chat_display.setTextColor(system_message_color)
+        self.chat_display.append("\n--------\n")
 
 
 # create the application
